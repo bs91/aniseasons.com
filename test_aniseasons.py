8e3d9616d18cb22774452ece88220089ffc8d1c4
hkal <_@hkal.me>
2013-04-07 03:24:21 -0400

    test skeleton

---
+++ test_aniseasons.py
@@ -0,0 +1,13 @@
+import os
+import aniseasons
+import unittest
+
+class AniseasonsTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
