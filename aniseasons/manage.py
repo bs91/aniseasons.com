e8c72dd806dae8a36f8876269dc7bc86d8a35d70
hkal <_@hkal.me>
2013-04-08 02:43:54 -0400

    display stdout using 'return' rather than 'print'

--- aniseasons/manage.py
+++ aniseasons/manage.py
@@ -10,10 +10,10 @@ def createuser(username, password):
     try:
         pass_hash = generate_password_hash(password)
         mongo.db.users.insert({'username': username, 'password': pass_hash})
-    except:
-        print 'Could not create user {0}'.format(username)
+    except e:
+        return 'Could not create user {0}'.format(username)
     else:
-        print '{0} was successfully created'.format(username)
+        return '{0} was successfully created'.format(username)
 
 
 @manager.command
@@ -21,9 +21,9 @@ def deleteuser(username):
     try:
         mongo.db.users.remove({'username': username})
     except:
-        print 'Could not delete user {0}'.format(username)
+        return 'Could not delete user {0}'.format(username)
     else:
-        print '{0} was successfully deleted'.format(username)
+        return '{0} was successfully deleted'.format(username)
 
 if __name__ == '__main__':
     manager.run()
