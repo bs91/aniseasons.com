c505cb6b47b5f0ffaa3d38c431db6ae868bd37b1
hkal <_@hkal.me>
2013-04-08 02:51:31 -0400

    added management tests

--- test_aniseasons.py
+++ test_aniseasons.py
@@ -1,6 +1,10 @@
+from flask.ext.pymongo import PyMongo
+
 import os
-import aniseasons
 import unittest
+
+from aniseasons.app import app, mongo
+from aniseasons.manage import createuser, deleteuser
 
 class AniseasonsTestCase(unittest.TestCase):
     def setUp(self):
@@ -8,6 +12,14 @@ class AniseasonsTestCase(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_createuser(self):
+        with app.app_context():
+            self.assertEqual(createuser('testington', 'test'), 'testington was successfully created')
+
+    def test_deleteuser(self):
+        with app.app_context():
+            self.assertEqual(deleteuser('testington'), 'testington was successfully deleted')
 
 if __name__ == '__main__':
     unittest.main()
