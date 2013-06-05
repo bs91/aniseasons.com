74ae562d0ba976063e90f3144d8b1b0351091da5
hkal <_@hkal.me>
2013-06-05 08:53:50 -0400

    refactor filters

--- aniseasons/aniseasons.py
+++ aniseasons/aniseasons.py
@@ -11,17 +11,6 @@ import os
 import re
 
 
-_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
-
-@app.template_filter()
-@evalcontextfilter
-def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
-        for p in _paragraph_re.split(escape(value)))
-    if eval_ctx.autoescape:
-        result = Markup(result)
-    return result
-
 # helper method for resizing images to proper size
 def resize_image(pic, max_width):
     im = Image.open(pic)
