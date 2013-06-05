0fc16d01e99c709b3e6fe8e2cdfce8987427b9b8
hkal <_@hkal.me>
2013-06-05 09:50:37 -0400

    move startup code to __init__

---
+++ aniseasons/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+
+from aniseasons.settings import Settings
+
+import os
+
+
+app = Flask(__name__)
+app.config.from_object(Settings)
+
+mongo = PyMongo(app)
+
+if not os.path.exists(app.config['UPLOAD_PATH']):
+    os.makedirs(app.config['UPLOAD_PATH'])
+    os.chmod(app.config['MEDIA_PATH'], 0775)
+    os.chmod(app.config['UPLOAD_PATH'], 0775)
+
+if app.config['DEBUG']:
+    from werkzeug import SharedDataMiddleware
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+        '/media': app.config['MEDIA_PATH'],
+        '/static': app.config['STATIC_PATH'],
+    })
