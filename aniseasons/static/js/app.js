f8fccfb92c7a7579ee0e635c28928451a2cbdecd
hkal <_@hkal.me>
2013-08-16 19:47:22 -0400

    add inclusive genre filtering

--- aniseasons/static/js/app.js
+++ aniseasons/static/js/app.js
@@ -1,7 +1,33 @@
 'use strict'
 
-var app = angular.module('aniseasons', ['aniseasons.services', 'aniseasons.file', 'wu.masonry', 'ui.bootstrap', 'ui.select2']).
-config(function($interpolateProvider){
-    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-  }
-);
+var app = angular.module('aniseasons', ['aniseasons.services', 'aniseasons.file', 'wu.masonry', 'ui.bootstrap', 'ui.select2']);
+
+app.config(function($interpolateProvider){
+  $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
+
+app.filter('genreFilter', function() {
+  return function(anime, genres) {
+    var items = {
+      genres: genres,
+      out: []
+    };
+
+    if (genres.length !== 0) {
+      angular.forEach(anime, function (value, key) {
+        for (var i = 0; i < this.genres.length; i++) {
+          if (value.genre.indexOf(this.genres[i]) !== -1) {
+            this.out.push(value);
+            break;
+          }
+        }
+      }, items);
+    } else {
+      items.out = anime;
+    }
+
+    return items.out
+  };
+});
+
+angular.bootstrap(document, ['aniseasons']);
