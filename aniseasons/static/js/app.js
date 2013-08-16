f2855d5dad22068467b19e4f9a4738b5e2bc0f3e
hkal <_@hkal.me>
2013-08-16 18:44:49 -0400

    use angular-ui-select2

--- aniseasons/static/js/app.js
+++ aniseasons/static/js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
-var app = angular.module('aniseasons', ['aniseasons.services', 'aniseasons.file', 'wu.masonry', 'ui.bootstrap']).
-  config(function($interpolateProvider){
+var app = angular.module('aniseasons', ['aniseasons.services', 'aniseasons.file', 'wu.masonry', 'ui.bootstrap', 'ui.select2']).
+config(function($interpolateProvider){
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
   }
 );
