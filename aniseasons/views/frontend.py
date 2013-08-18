6b8c8b731dd098ae7e77746d77edd8441e127ebb
hkal <_@hkal.me>
2013-08-18 15:04:27 -0400

    dynamically create genre and year lists

--- aniseasons/views/frontend.py
+++ aniseasons/views/frontend.py
@@ -1,8 +1,24 @@
 from flask import Blueprint, render_template
 
+from aniseasons import mongo
+
 mod = Blueprint('frontend', __name__)
 
 
 @mod.route('/')
 def index():
-    return render_template("index.html")
+    years = []
+    genres = []
+
+    anime = mongo.db.anime.find()
+
+    # make this into a funky list comprehension when less brain-dead
+    for entry in anime:
+        for genre in entry['genre']:
+            if genre not in genres:
+                genres.append(genre)
+
+        if entry['year'] not in years and entry['year']:
+            years.append(entry['year'])
+
+    return render_template("index.html", years=years, genres=genres)
